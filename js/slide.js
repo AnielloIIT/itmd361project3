4c34f97eb40b218cb4f79c88e62dbe10332df845
Aniello Scartozzi <ascartoz@hawk.iit.edu>
2017-04-25 18:48:39 -0500

    changed speed of slider and cleaned up code

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,24 @@
-
-
+// Function for jquery slider
 $(function() {
     $(".rslides").responsiveSlides();
   });
+
+// Function to customize slider
+$(".rslides").responsiveSlides({
+  auto: true,             // Auto animation
+  speed: 500,             // Length of window animation
+  timeout: 5000,          // Time between image transitions
+  pager: false,           // This will display the pager
+  nav: false,             // This will display the navigation bar
+  random: false,          // Makes imgaes appear randomly
+  pause: false,           // Makes slideshow pause when mouse is hovering over slideshow
+  pauseControls: false,   // This will add a button to pause the slideshow
+  prevText: "Previous",   // Adds a previous text button
+  nextText: "Next",       // Adds a next text button
+  maxwidth: "",           // This sets the width of the slideshow in pixels
+  navContainer: "",       // This sets where the navigation bar is appended to, default is after the 'ul'
+  manualControls: "",     // This is used to set custom navigation
+  namespace: "rslides",   // This is used to change the default namesapce
+  before: function(){},   // Before callback function
+  after: function(){}     // After callback function
+});
